d79afaaeec2b65f2ade7ca38ab2051777f564bae
jpileto <jp.ileto@icloud.com>
2016-08-09 15:59:34 -0400

    Start exercise 4

--- exercise3.rb
+++ exercise3.rb
@@ -1,8 +1,10 @@
 puts "What is your name?"
 name = gets.chomp
 puts "Hi #{name}"
+# Get and print name with greeting
 
 puts "How old are you?"
 age = gets.chomp
 year = 2016
 puts "You were born on #{year - age.to_i}"
+# Get the age and determine the year
