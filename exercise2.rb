dc09289b75a31fb83c11d470ae252f2431f2c18a
jpileto <jp.ileto@icloud.com>
2016-08-09 14:57:02 -0400

    Finish exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,20 @@
+tip = "(55*10)"
+# 10% or more tip is acceptable
+s = "5"
+if s < tip
+  puts "Not enough"
+else
+  puts "Great tip!"
+end
+
+e = "17"
+# puts e + 2
+# Convert integer into string
+puts e + 2.to_s
+
+puts "He won #{45628 * 7839} in the lottery yesterday."
+
+puts (10 < 20) && (30 < 20) || !(10 == 11)
+# (true) and (false) or true
+# false or true
+# true 
